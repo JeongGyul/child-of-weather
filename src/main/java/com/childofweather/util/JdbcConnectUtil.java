b5acfff712cb757bbfaa712932833521ab320493
JeongGyul <jjascd@naver.com>
2025-12-01 00:26:25 +0900

    chore: DB 접속 주소 변경

--- src/main/java/com/childofweather/util/JdbcConnectUtil.java
+++ src/main/java/com/childofweather/util/JdbcConnectUtil.java
@@ -1,12 +1,31 @@
 package com.childofweather.util;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class JdbcConnectUtil {
+	
+	private static final Properties properties = new Properties();
+
+	static {
+	    try (InputStream input = JdbcConnectUtil.class.getClassLoader().getResourceAsStream("db.properties")) {
+	        if (input == null) {
+	            // 파일을 찾지 못하면 에러 발생
+	            System.out.println("Sorry, unable to find db.properties");
+	            throw new FileNotFoundException("db.properties 파일을 찾을 수 없습니다.");
+	        }
+	        // db.properties 파일 내용을 로드
+	        properties.load(input);
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	}
 
     public static Connection getConnection() {
         Connection conn = null;
@@ -15,9 +34,9 @@ public class JdbcConnectUtil {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // ⚠️ 네 DB 환경에 맞게 수정
-            String url = "jdbc:mysql://localhost:3306/project";
-            String user = "root";
-            String password = "0000";
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
 
             conn = DriverManager.getConnection(url, user, password);
         } catch (ClassNotFoundException | SQLException e) {
